077660e63c88dc06609c7a95e15bb45db1cbdafc
adrian <bontojohnadrian@gmail.com>
2025-11-27 20:18:21 +0800

    feat: make landing page responsive

--- app/(landing)/landing_page/home/page.tsx
+++ app/(landing)/landing_page/home/page.tsx
@@ -1,58 +1,3 @@
-// import Image from "next/image";
-
-// const LandingHomePage = () => {
-//   return (
-//     <div className="relative h-screen overflow-hidden bg-gradient-to-t from-[#e3ae01] via-[#fed795] to-[#fcf4d2]">
-//       <h1 className="font-serif font-extrabold text-3xl text-center mt-5 text-[#173f63]">
-//         Welcome to CTIC!
-//       </h1>
-//       <div className="flex items-center justify-center py-10">
-//       <div className="flex flex-row ml-5">
-//         <Image
-//               src="/cticlogo.webp"
-//               height={350}
-//               width={350}
-//               alt="CTIC"
-//               className="object-contain"
-//             />
-//         <div className="flex flex-col">
-//         <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
-//           Canlubang Techno-Industrial Corporation
-//         </div>
-//         <h2 className="text-[#173f63] font-extralight mt-4">
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus necessitatibus earum
-//         </h2>
-//         <br></br>
-//         <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
-//           Background
-//         </div>
-//         <div className="text-[#173f63] font-extralight mt-4">
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus necessitatibus earum...
-//         </div>
-//         <br></br>
-//         <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
-//           Partnerships
-//         </div>
-//         <h2 className="text-[#173f63] font-extralight mt-4">
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus necessitatibus earum...
-//         </h2>
-//         <br></br>
-//         <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
-//           Clients
-//         </div>
-//         <div className="text-[#173f63] font-extralight mt-4">
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus necessitatibus earum...
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-//     </div>
-//   );
-// };
-
-// export default LandingHomePage;
-
-
 import Image from "next/image";
 import { Header } from "../../header";
 
@@ -69,54 +14,54 @@ const LandingHomePage = () => {
       "
     >
       <Header />
-      <div className="font-serif font-extrabold text-3xl text-center mt-5 text-[#173f63]">
+      <div className="font-serif font-extrabold text-2xl sm:text-3xl md:text-4xl text-center mt-5 px-4 text-[#173f63]">
         Welcome to CTIC!
       </div>
 
-      <div className="flex flex-col md:flex-row items-center justify-center py-8 px-4 gap-4">
+      <div className="flex flex-col md:flex-row items-center justify-center py-8 px-4 gap-6 md:gap-8 max-w-7xl mx-auto">
         <Image
           src="/cticlogo.webp"
           alt="CTIC"
           width={350}
           height={350}
-          className="w-full max-w-[300px] h-auto object-contain"
+          className="w-full max-w-[250px] sm:max-w-[300px] md:max-w-[350px] h-auto object-contain"
         />
 
-        <div className="flex flex-col max-w-2xl text-center md:text-left">
-          <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
+        <div className="flex flex-col max-w-2xl text-center md:text-left px-4">
+          <div className="text-xl sm:text-2xl font-serif font-semibold text-[#173f63] mt-2">
             Canlubang Techno-Industrial Corporation
           </div>
-          <p className="text-[#173f63] font-extralight mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla
-            recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus
-            necessitatibus earum.
+          <p className="text-[#173f63] font-extralight mt-4 text-sm sm:text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem
+            iure ab expedita repellendus necessitatibus earum.
           </p>
 
-          <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
+          <div className="text-xl sm:text-2xl font-serif font-semibold text-[#173f63] mt-6">
             Background
           </div>
-          <p className="text-[#173f63] font-extralight mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla
-            recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus
-            necessitatibus earum...
+          <p className="text-[#173f63] font-extralight mt-4 text-sm sm:text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem
+            iure ab expedita repellendus necessitatibus earum...
           </p>
 
-          <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
+          <div className="text-xl sm:text-2xl font-serif font-semibold text-[#173f63] mt-6">
             Partnerships
           </div>
-          <p className="text-[#173f63] font-extralight mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla
-            recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus
-            necessitatibus earum...
+          <p className="text-[#173f63] font-extralight mt-4 text-sm sm:text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem
+            iure ab expedita repellendus necessitatibus earum...
           </p>
 
-          <div className="text-2xl font-serif font-semibold text-[#173f63] mt-2">
+          <div className="text-xl sm:text-2xl font-serif font-semibold text-[#173f63] mt-6">
             Clients
           </div>
-          <p className="text-[#173f63] font-extralight mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque eum, omnis nulla
-            recusandae obcaecati quaerat aut dicta autem iure ab expedita repellendus
-            necessitatibus earum...
+          <p className="text-[#173f63] font-extralight mt-4 text-sm sm:text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            cumque eum, omnis nulla recusandae obcaecati quaerat aut dicta autem
+            iure ab expedita repellendus necessitatibus earum...
           </p>
         </div>
       </div>
